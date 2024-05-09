a9d90516f2842ca676f502e338f7ce42f222c9c5
Marat Bakirov <marat@deovr.com>
2024-05-09 13:02:43 +0200

    Add CI/CD and update goofys version

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,26 @@
-FROM alpine:3.7
+FROM alpine:latest
 
-MAINTAINER Cloud Posse, LLC
+MAINTAINER Marat Bakirov
 
 RUN apk update && apk add gcc ca-certificates openssl musl-dev git fuse syslog-ng coreutils curl
 
-ENV GOOFYS_VERSION 0.23.1
-RUN curl --fail -sSL -o /usr/local/bin/goofys https://github.com/kahing/goofys/releases/download/v${GOOFYS_VERSION}/goofys \
-    && chmod +x /usr/local/bin/goofys
-RUN curl -sSL -o /usr/local/bin/catfs https://github.com/kahing/catfs/releases/download/v0.8.0/catfs && chmod +x /usr/local/bin/catfs
+ENV GOOFYS_VERSION 0.24.0
+RUN curl --fail -sSL -o /usr/local/bin/goofys https://github.com/kahing/goofys/releases/download/v${GOOFYS_VERSION}/goofys && \
+    chmod +x /usr/local/bin/goofys; \
+    curl -sSL -o /usr/local/bin/catfs https://github.com/kahing/catfs/releases/download/v0.8.0/catfs && \
+    chmod +x /usr/local/bin/catfs;
 
-ARG ENDPOINT
+ENV ENDPOINT https://s3.amazonaws.com
 ENV MOUNT_DIR /mnt/s3
-ENV REGION us-east-1
+ENV REGION ""
 ENV BUCKET teleport-bucket
-ENV STAT_CACHE_TTL 1m0s
-ENV TYPE_CACHE_TTL 1m0s
+ENV STAT_CACHE_TTL ""
+ENV TYPE_CACHE_TTL ""
 ENV DIR_MODE 0700
 ENV FILE_MODE 0600
 ENV UID 0
 ENV GID 0
+ENV ACL private
 
 RUN mkdir /mnt/s3
 
--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -2,6 +2,50 @@
 
 syslog-ng -f /etc/syslog-ng/syslog-ng.conf
 
-[[ "$UID" -ne "0" ]] && MOUNT_ACCESS="allow_other" || MOUNT_ACCESS="nonempty"
+GOOFYS_CMD="/usr/local/bin/goofys";
 
-goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS $BUCKET $MOUNT_DIR
+if [ "$UID" -ne "0" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --uid $UID";
+fi
+
+if [ "$GID" -ne "0" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --gid $GID";
+fi
+
+if [ -n "$DIR_MODE" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --dir-mode $DIR_MODE";
+fi
+
+if [ -n "$FILE_MODE" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --file-mode $FILE_MODE";
+fi
+
+if [ -n "$ACL" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --acl $ACL";
+fi
+
+if [ -n "$ENDPOINT" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --endpoint $ENDPOINT";
+fi
+
+if [ -n "$STAT_CACHE_TTL" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --stat-cache-ttl $STAT_CACHE_TTL";
+fi
+
+if [ -n "$TYPE_CACHE_TTL" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD --type-cache-ttl $TYPE_CACHE_TTL";
+fi
+
+if [ -n "$MOUNT_ACCESS" ]; then
+  GOOFYS_CMD="$GOOFYS_CMD -o $MOUNT_ACCESS";
+elif [[ "$UID" -ne "0" ]]; then
+  GOOFYS_CMD="$GOOFYS_CMD -o allow_other";
+else
+  GOOFYS_CMD="$GOOFYS_CMD -o nonempty";
+fi
+
+GOOFYS_CMD="$GOOFYS_CMD -f $BUCKET $MOUNT_DIR";
+
+echo "Goofys start command is: [$GOOFYS_CMD]..." > /dev/stdout
+
+$GOOFYS_CMD
